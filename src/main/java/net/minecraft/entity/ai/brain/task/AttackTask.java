f2d58d9ef8794a44de51d212e7301df0d0c68723
Real Name <DIOJOIO41@163.com>
2025-02-02 23:36:13 +0800

    kingdom cum

--- src/main/java/net/minecraft/entity/ai/brain/task/AttackTask.java
+++ src/main/java/net/minecraft/entity/ai/brain/task/AttackTask.java
@@ -8,6 +8,12 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.util.math.MathHelper;
 
 public class AttackTask {
+	/**
+	 * 处理怪物的攻击逻辑
+	 * @param distance 攻击范围，在这个范围内才会触发攻击
+	 * @param forwardMovement 前后移动距离(貌似负数为倒退)
+	 * @return 一个单次触发的任务(SingleTickTask<MobEntity>),用于在特定条件下触发攻击行为
+	 */
 	public static SingleTickTask<MobEntity> create(int distance, float forwardMovement) {
 		return TaskTriggerer.task(
 			context -> context.group(
@@ -17,14 +23,14 @@ public class AttackTask {
 						context.queryMemoryValue(MemoryModuleType.VISIBLE_MOBS)
 					)
 					.apply(context, (walkTarget, lookTarget, attackTarget, visibleMobs) -> (world, entity, time) -> {
-							LivingEntity livingEntity = context.getValue(attackTarget);
-							if (livingEntity.isInRange(entity, (double)distance) && context.<LivingTargetCache>getValue(visibleMobs).contains(livingEntity)) {
-								lookTarget.remember(new EntityLookTarget(livingEntity, true));
-								entity.getMoveControl().strafeTo(-forwardMovement, 0.0F);
-								entity.setYaw(MathHelper.clampAngle(entity.getYaw(), entity.headYaw, 0.0F));
-								return true;
+							LivingEntity livingEntity = context.getValue(attackTarget);//获取攻击目标
+							if (livingEntity.isInRange(entity, distance) && context.getValue(visibleMobs).contains(livingEntity)) {//判断攻击目标是否在攻击范围内
+								lookTarget.remember(new EntityLookTarget(livingEntity, true));//记住攻击目标
+								entity.getMoveControl().strafeTo(-forwardMovement, 0.0F);//向攻击目标移动(倒退?)
+								entity.setYaw(MathHelper.clampAngle(entity.getYaw(), entity.headYaw, 0.0F));//调整朝向，使身体朝向与头部朝向一致
+								return true;//返回true表示任务执行成功
 							} else {
-								return false;
+								return false;//返回false表示不满足inRange条件
 							}
 						})
 		);
--- src/main/java/net/minecraft/entity/ai/brain/task/ForgetTask.java
+++ src/main/java/net/minecraft/entity/ai/brain/task/ForgetTask.java
@@ -5,10 +5,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 
 public class ForgetTask {
-	public static <E extends LivingEntity> Task<E> create(Predicate<E> condition, MemoryModuleType<?> memory) {
+	/**
+	 *如果实体满足条件，则忘记记忆。
+	 * @param condition 测试实体是否应该忘记记忆的条件
+	 * @param memory 要忘记的记忆类型
+	 * @return 是否忘记记忆
+	 * @param <E> 实体类型(LivingEntity的子类)
+	 */
+	public static <E extends LivingEntity> Task<E> create(Predicate<E> condition, MemoryModuleType<?> memory) {//下面的lambda表达式不想看了
 		return TaskTriggerer.task(context -> context.group(context.queryMemoryValue(memory)).apply(context, queryResult -> (world, entity, time) -> {
-					if (condition.test(entity)) {
-						queryResult.forget();
+					if (condition.test(entity)) {//判断实体是否满足遗忘条件
+						queryResult.forget();//遗忘记忆
 						return true;
 					} else {
 						return false;
